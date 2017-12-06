f35eaf46b94ee25811c8ca8f377a41e450871e35
renhy7 <32011547+renhy7@users.noreply.github.com>
2017-12-06 21:04:41 +0800

    Update NAHomeCourse.java

--- src/main/java/TestScript/NAHomeCourse.java
+++ src/main/java/TestScript/NAHomeCourse.java
@@ -1,23 +1,56 @@
 package TestScript;
 
-import org.testng.ITestContext;
+import java.util.Arrays;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import TestData.HomeCourseData;
 import CommonFunction.Common;
 import CommonFunction.SuperTestClass;
+import Logger.Dailylog;
+import TestData.CommonData;
+import TestData.HomeCourseData;
 
 public class NAHomeCourse extends SuperTestClass {
 	
+	
 	@Test
-	public void NAHomeCourseTest(ITestContext ctx){
-		try{
-			System.out.println(Common.sendPost(HomeCourseData.getUrl() , "id="+userMobile+"&x="+HomeCourseData.getLongitude()+"&y="+HomeCourseData.getLatitude()));
-			
-		}catch(Throwable e){
-			e.printStackTrace();
-		}
+	public void NAHomeCourseTest(){
 		
+			String response = Common.sendPost(HomeCourseData.getUrl() , "id="+CommonData.getUserId(userMobile)+"&x="+HomeCourseData.getLongitude()+"&y="+HomeCourseData.getLatitude());
+			JSONObject jsonResponse = JSONObject.fromObject(response);
+			Dailylog.logInfo("NAHomeCourse case response data:"+response);
+			
+			JSONArray responseCommon =  jsonResponse.getJSONArray("common");
+			String commonId[] = new String[responseCommon.size()];
+		    for(int i = 0; i< responseCommon.size();i++){
+		    	String str = responseCommon.getString(i);
+		    	JSONObject obj = JSONObject.fromObject(str);
+		    	Dailylog.logInfo("NAHomeCourse case commonId:"+obj.get("id"));
+		    	commonId[i] = obj.get("id").toString();
+		    }				
+			JSONArray responseRecomme = jsonResponse.getJSONArray("recomme");
+			String recommeId[] = new String[responseRecomme.size()];
+			for(int i = 0; i < responseRecomme.size(); i++){
+				String str = responseRecomme.getString(i);
+				JSONObject obj = JSONObject.fromObject(str);
+				Dailylog.logInfo("NAHomeCourse case recommeId:"+obj.getString("id"));
+				recommeId[i] = obj.getString("id").toString();
+			}
+			for(int i = 0 ; i< commonId.length; i++){
+				Dailylog.logInfo("commonId:"+commonId[i]);
+			}
+			for(int j = 0; j< recommeId.length;j++){
+				Dailylog.logInfo("recommeId:"+recommeId[j]);
+			}
+			Arrays.sort(commonId);
+			Arrays.sort(recommeId);
+			System.out.println(Arrays.equals(commonId, recommeId));
+			Assert.assertTrue(Arrays.equals(commonId, recommeId), "commonId != recommeId");
+			
 	}
 
 }
